bddd489c45244afa943e4af4f8565af02a3fec4e
ludovic <ludovicdebever0@gmail.com>
2024-03-10 12:07:05 +0100

    chore(tests): initializing tests

---
+++ Tests/Lexer.Test.cs
@@ -0,0 +1,12 @@
+namespace Rhino.Tests;
+
+public class LexerTest {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void Test1() {
+        Assert.Pass();
+    }
+}
---
+++ Tests/Parser.Test.cs
@@ -0,0 +1,11 @@
+namespace Rhino.Tests;
+
+public class ParserTest {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void Test1() {
+    }
+}
