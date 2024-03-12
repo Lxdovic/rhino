d8a755ebb8ea30ac46c570afbd1d67de9383cf91
ludovic <ludovicdebever0@gmail.com>
2024-03-12 20:20:01 +0100

    feat: added variables

---
+++ Rhino/CodeAnalysis/VariableSymbol.cs
@@ -0,0 +1,11 @@
+namespace Rhino.CodeAnalysis;
+
+public sealed class VariableSymbol {
+    public VariableSymbol(string name, Type type) {
+        Name = name;
+        Type = type;
+    }
+
+    public string Name { get; }
+    public Type Type { get; }
+}
