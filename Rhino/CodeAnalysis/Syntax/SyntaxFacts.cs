917078588859b0c633fad6bec2153959bc30f9b1
ludovic <ludovicdebever0@gmail.com>
2024-03-14 23:17:26 +0100

    feat: add GetBinaryOperatorKinds & GetUnaryOperatorKinds to SyntaxFacts.cs

--- Rhino/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ Rhino/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -36,7 +36,7 @@ public static class SyntaxFacts {
             case SyntaxKind.TildeToken:
                 return 6;
             default:
-                return 0;
+                return 0; // not a unary operator
         }
     }
 
@@ -77,5 +77,21 @@ public static class SyntaxFacts {
             case SyntaxKind.GreaterThanOrEqualsToken: return ">=";
             default: return null;
         }
+    }
+
+    public static IEnumerable<SyntaxKind> GetBinaryOperatorKinds() {
+        var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+
+        foreach (var kind in kinds)
+            if (GetBinaryOperatorPrecedence(kind) > 0)
+                yield return kind;
+    }
+
+    public static IEnumerable<SyntaxKind> GetUnaryOperatorKinds() {
+        var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+
+        foreach (var kind in kinds)
+            if (GetUnaryOperatorPrecedence(kind) > 0)
+                yield return kind;
     }
 }
