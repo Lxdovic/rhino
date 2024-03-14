3fadf1e4df7eadba2b680e524db7aa1ff38135e4
Lxdovic <ludovicdebever0@gmail.com>
2024-03-14 19:45:12 +0100

    tests: added tests for bitwise operators

--- Rhino.Tests/CodeAnalysis/Syntax/LexerTest.cs
+++ Rhino.Tests/CodeAnalysis/Syntax/LexerTest.cs
@@ -73,16 +73,16 @@ public class LexerTest {
 
         if (t1Kind == SyntaxKind.EqualsToken && t2Kind == SyntaxKind.EqualsEqualsToken)
             return true;
-        
+
         if (t1Kind == SyntaxKind.BinaryAndToken && t2Kind == SyntaxKind.AmpersandAmpersandToken)
             return true;
-        
+
         if (t1Kind == SyntaxKind.BinaryOrToken && t2Kind == SyntaxKind.PipePipeToken)
             return true;
-        
+
         if (t1Kind == SyntaxKind.BinaryAndToken && t2Kind == SyntaxKind.BinaryAndToken)
             return true;
-        
+
         if (t1Kind == SyntaxKind.BinaryOrToken && t2Kind == SyntaxKind.BinaryOrToken)
             return true;
 
@@ -127,6 +127,7 @@ public class LexerTest {
             (SyntaxKind.HatToken, "^"),
             (SyntaxKind.LessThanLessThanToken, "<<"),
             (SyntaxKind.GreaterThanGreaterThanToken, ">>"),
+            (SyntaxKind.TildeToken, "~"),
 
             (SyntaxKind.NumberToken, "1"),
             (SyntaxKind.NumberToken, "123"),
