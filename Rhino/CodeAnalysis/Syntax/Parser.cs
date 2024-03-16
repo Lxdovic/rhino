9be09f5331bcafcb3fcb21c07a14b8577494447d
ludovic <ludovicdebever0@gmail.com>
2024-03-16 15:09:02 +0100

    refactor(parser): cleanup better code readability

--- Rhino/CodeAnalysis/Syntax/Parser.cs
+++ Rhino/CodeAnalysis/Syntax/Parser.cs
@@ -19,9 +19,9 @@ internal sealed class Parser {
         Diagnostics.AddRange(lexer.Diagnostics);
     }
 
-    private SyntaxToken Current => Peek(0);
-
     public DiagnosticBag Diagnostics { get; } = new();
+
+    private SyntaxToken Current => Peek(0);
 
     private SyntaxToken Peek(int offset) {
         var index = _position + offset;
@@ -92,29 +92,46 @@ internal sealed class Parser {
     private ExpressionSyntax ParsePrimaryExpression() {
         switch (Current.Kind) {
             case SyntaxKind.OpenParenthesisToken: {
-                var left = NextToken();
-                var expression = ParseExpression();
-                var right = MatchToken(SyntaxKind.CloseParenthesisToken);
-                return new ParenthesizedExpressionSyntax(left, expression, right);
+                return ParseParenthesizedExpression();
             }
             case SyntaxKind.TrueKeyword:
             case SyntaxKind.FalseKeyword: {
-                var keywordToken = NextToken();
-                var value = keywordToken.Kind == SyntaxKind.TrueKeyword;
-
-                return new LiteralExpressionSyntax(keywordToken, value);
+                return ParseBooleanLiteral();
             }
-            case SyntaxKind.IdentifierToken: {
-                var identifierToken = NextToken();
-
-                return new NameExpressionSyntax(identifierToken);
+            case SyntaxKind.NumberToken: {
+                return ParseNumberLiteral();
             }
+
+            case SyntaxKind.IdentifierToken:
 
             default: {
-                var numberToken = MatchToken(SyntaxKind.NumberToken);
-
-                return new LiteralExpressionSyntax(numberToken);
+                return ParseNameExpression();
             }
         }
+    }
+
+    private ExpressionSyntax ParseNumberLiteral() {
+        var numberToken = MatchToken(SyntaxKind.NumberToken);
+
+        return new LiteralExpressionSyntax(numberToken);
+    }
+
+    private ExpressionSyntax ParseParenthesizedExpression() {
+        var left = MatchToken(SyntaxKind.OpenParenthesisToken);
+        var expression = ParseExpression();
+        var right = MatchToken(SyntaxKind.CloseParenthesisToken);
+        return new ParenthesizedExpressionSyntax(left, expression, right);
+    }
+
+    private ExpressionSyntax ParseBooleanLiteral() {
+        var isTrue = Current.Kind == SyntaxKind.TrueKeyword;
+        var keywordToken = isTrue ? MatchToken(SyntaxKind.TrueKeyword) : MatchToken(SyntaxKind.FalseKeyword);
+
+        return new LiteralExpressionSyntax(keywordToken, isTrue);
+    }
+
+    private ExpressionSyntax ParseNameExpression() {
+        var identifierToken = MatchToken(SyntaxKind.IdentifierToken);
+        return new NameExpressionSyntax(identifierToken);
     }
 }
