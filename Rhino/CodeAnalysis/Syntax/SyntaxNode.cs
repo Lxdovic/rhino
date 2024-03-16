0501efbc06875736795ee7ca121e9a6c4d012b5c
ludovic <ludovicdebever0@gmail.com>
2024-03-16 20:23:07 +0100

    chore: move PrettyPrint to SyntaxNode.cs

--- Rhino/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Rhino/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -24,4 +24,35 @@ public abstract class SyntaxNode {
                 foreach (var child in (IEnumerable<SyntaxNode>)property.GetValue(this))
                     yield return child;
     }
+
+    public void WriteTo(TextWriter writer) {
+        PrettyPrint(writer, this);
+    }
+
+    private static void PrettyPrint(TextWriter writer, SyntaxNode node, string indent = "", bool isLast = true) {
+        var marker = isLast ? "└──" : "├──";
+
+        Console.Write(indent);
+        Console.Write(marker);
+        Console.Write(node.Kind);
+
+        if (node is SyntaxToken t && t.Value != null) {
+            Console.Write(" ");
+            Console.Write(t.Value);
+        }
+
+        Console.WriteLine();
+
+        indent += isLast ? "   " : "│  ";
+
+        var lastChild = node.GetChildren().LastOrDefault();
+
+        foreach (var child in node.GetChildren()) PrettyPrint(writer, child, indent, child == lastChild);
+    }
+
+    public override string ToString() {
+        using var writer = new StringWriter();
+        WriteTo(writer);
+        return writer.ToString();
+    }
 }
--- mc/Program.cs
+++ mc/Program.cs
@@ -7,7 +7,7 @@ internal static class Program {
     private static void Main(string[] args) {
         var showTree = false;
         var variables = new Dictionary<VariableSymbol, object>();
-        
+
         while (true) {
             Console.Write("> ");
             var line = Console.ReadLine();
@@ -29,13 +29,13 @@ internal static class Program {
             var compilation = new Compilation(syntaxTree);
             var result = compilation.Evaluate(variables);
             var diagnostics = result.Diagnostics;
-            
+
             if (showTree) {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                PrettyPrint(syntaxTree.Root);
+                syntaxTree.Root.WriteTo(Console.Out);
                 Console.ResetColor();
             }
-            
+
             if (!diagnostics.Any()) {
                 Console.WriteLine(result.Value);
             }
@@ -46,47 +46,26 @@ internal static class Program {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine(diagnostic);
                     Console.ResetColor();
-                    
+
                     var prefix = line.Substring(0, diagnostic.Span.Start);
                     var error = line.Substring(diagnostic.Span.Start, diagnostic.Span.Length);
                     var suffix = line.Substring(diagnostic.Span.End);
-                    
+
                     Console.Write("    ");
                     Console.Write(prefix);
-                    
+
                     Console.ForegroundColor = ConsoleColor.DarkRed;
-                    
+
                     Console.Write(error);
-                    
+
                     Console.ResetColor();
-                    
+
                     Console.Write(suffix);
                     Console.WriteLine();
                 }
-                
+
                 Console.WriteLine();
             }
         }
-    }
-
-    private static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true) {
-        var marker = isLast ? "└──" : "├──";
-
-        Console.Write(indent);
-        Console.Write(marker);
-        Console.Write(node.Kind);
-
-        if (node is SyntaxToken t && t.Value != null) {
-            Console.Write(" ");
-            Console.Write(t.Value);
-        }
-
-        Console.WriteLine();
-
-        indent += isLast ? "   " : "│  ";
-
-        var lastChild = node.GetChildren().LastOrDefault();
-
-        foreach (var child in node.GetChildren()) PrettyPrint(child, indent, child == lastChild);
     }
 }
