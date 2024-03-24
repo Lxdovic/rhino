76d5d149c8e58b881d5da0912d2311ad64a23b32
ludovic <ludovicdebever0@gmail.com>
2024-03-24 13:20:57 +0100

    fix(syntaxNode): fixed pretty printing failing with null values

--- Rhino/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Rhino/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -18,12 +18,17 @@ public abstract class SyntaxNode {
         var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
         foreach (var property in properties)
-            if (typeof(SyntaxNode).IsAssignableFrom(property.PropertyType))
-                yield return (SyntaxNode)property.GetValue(this);
+            if (typeof(SyntaxNode).IsAssignableFrom(property.PropertyType)) {
+                var child = (SyntaxNode)property.GetValue(this);
 
-            else if (typeof(IEnumerable<SyntaxNode>).IsAssignableFrom(property.PropertyType))
+                if (child != null) yield return child;
+            }
+
+            else if (typeof(IEnumerable<SyntaxNode>).IsAssignableFrom(property.PropertyType)) {
                 foreach (var child in (IEnumerable<SyntaxNode>)property.GetValue(this))
-                    yield return child;
+                    if (child != null)
+                        yield return child;
+            }
     }
 
     public void WriteTo(TextWriter writer) {
