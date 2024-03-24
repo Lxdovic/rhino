b520c7689307de4854cd8c9d57ea85984c13f711
ludovic <ludovicdebever0@gmail.com>
2024-03-24 12:04:05 +0100

    tests(evaluator): added tests for diagnostics

--- Rhino.Tests/CodeAnalysis/EvaluationTests.cs
+++ Rhino.Tests/CodeAnalysis/EvaluationTests.cs
@@ -40,7 +40,98 @@ public class EvaluationTests {
     [InlineData("true || false", true)]
     [InlineData("false || true", true)]
     [InlineData("false || false", false)]
-    public void SyntaxFactGetTextRoundTrips(string text, object expectedValue) {
+    public void EvaluatorComputesCorrectValues(string text, object expectedValue) {
+        AssertValue(text, expectedValue);
+    }
+
+    [Fact]
+    public void EvaluatorVariableDeclarationReportsRedeclaration() {
+        var text = @"
+            {
+                var x = 10
+                var y = 100
+                {
+                    var x = 10
+                }
+                var [x] = 5
+            }
+        ";
+
+        var diagnostics = @"
+            ERROR: variable 'x' is already declared.";
+
+        AssertDiagnostics(text, diagnostics);
+    }
+
+    [Fact]
+    public void EvaluatorNameReportsUndefined() {
+        var text = @"[x] * 10";
+
+        var diagnostics = @"
+            ERROR: variable 'x' doesn't exist.";
+
+        AssertDiagnostics(text, diagnostics);
+    }
+
+    [Fact]
+    public void EvaluatorAssignmentReportsUndefined() {
+        var text = @"[x] * 10";
+
+        var diagnostics = @"
+            ERROR: variable 'x' doesn't exist.";
+
+        AssertDiagnostics(text, diagnostics);
+    }
+
+    [Fact]
+    public void EvaluatorAssignmentReportsCannotAssign() {
+        var text = @"
+            {
+                let x = 10
+                x [=] 10
+            }";
+
+        var diagnostics = @"
+            ERROR: cannot assign to variable 'x' because it is read-only.";
+
+        AssertDiagnostics(text, diagnostics);
+    }
+
+    [Fact]
+    public void EvaluatorAssignmentReportsCannotConvert() {
+        var text = @"
+            {
+                var x = 10
+                x = [true]
+            }";
+
+        var diagnostics = @"
+            ERROR: cannot convert type <System.Boolean> to <System.Int32>.";
+
+        AssertDiagnostics(text, diagnostics);
+    }
+
+    [Fact]
+    public void EvaluatorUnaryReportsUndefined() {
+        var text = @"[+]true";
+
+        var diagnostics = @"
+            Unary operator '+' is not defined for type <System.Boolean>";
+
+        AssertDiagnostics(text, diagnostics);
+    }
+
+    [Fact]
+    public void EvaluatorBinaryReportsUndefined() {
+        var text = @"10 [*] false";
+
+        var diagnostics = @"
+            Binary operator '*' is not defined for types <System.Int32> and <System.Boolean>";
+
+        AssertDiagnostics(text, diagnostics);
+    }
+
+    private static void AssertValue(string text, object expectedValue) {
         var expression = SyntaxTree.Parse(text);
         var compilation = new Compilation(expression);
         var variables = new Dictionary<VariableSymbol, object>();
@@ -48,5 +139,29 @@ public class EvaluationTests {
 
         Assert.Empty(result.Diagnostics);
         Assert.Equal(expectedValue, result.Value);
+    }
+
+    private void AssertDiagnostics(string text, string diagnosticText) {
+        var annotatedText = AnnotatedText.Parse(text);
+        var syntaxTree = SyntaxTree.Parse(annotatedText.Text);
+        var compilation = new Compilation(syntaxTree);
+        var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+        var expectedDiagnostics = AnnotatedText.UnindentLines(diagnosticText);
+
+        Assert.Equal(annotatedText.Spans.Length, expectedDiagnostics.Length);
+        Assert.Equal(expectedDiagnostics.Length, result.Diagnostics.Length);
+
+        for (var i = 0; i < expectedDiagnostics.Length; i++) {
+            var expectedMessage = expectedDiagnostics[i];
+            var actualMessage = result.Diagnostics[i].Message;
+
+            Assert.Equal(expectedMessage, actualMessage);
+
+            var expectedSpan = annotatedText.Spans[i];
+            var actualSpan = result.Diagnostics[i].Span;
+
+            Assert.Equal(expectedSpan, actualSpan);
+        }
     }
 }
