1d083266aa11b1cb39d768ab22c20cb29f1b55d8
ludovic <ludovicdebever0@gmail.com>
2024-03-28 21:10:04 +0100

    chore: moved BoundVariableDeclaration to it's own file

---
+++ Rhino/CodeAnalysis/Binding/BoundVariableDeclaration.cs
@@ -0,0 +1,12 @@
+namespace Rhino.CodeAnalysis.Binding;
+
+internal sealed class BoundVariableDeclaration : BoundStatement {
+    public BoundVariableDeclaration(VariableSymbol variable, BoundExpression initializer) {
+        Variable = variable;
+        Initializer = initializer;
+    }
+
+    public override BoundNodeKind Kind => BoundNodeKind.VariableDeclaration;
+    public VariableSymbol Variable { get; }
+    public BoundExpression Initializer { get; }
+}
