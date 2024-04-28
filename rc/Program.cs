5c3b9c805d6b8dd1985664bd8250d2cdd799749c
ludovic <ludovicdebever0@gmail.com>
2024-04-28 20:29:43 +0200

    fix(compiler): uncomment args handling code

--- rc/Program.cs
+++ rc/Program.cs
@@ -7,24 +7,24 @@ namespace rc;
 
 internal class Program {
     private static void Main(string[] args) {
-        // if (args.Length == 0) {
-        //     Console.WriteLine("Usage: rc <filename>");
-        //     return;
-        // }
+        if (args.Length == 0) {
+            Console.WriteLine("Usage: rc <filename>");
+            return;
+        }
 
-        // if (args.Length > 1) {
-        //     Console.WriteLine("Error: only one path supported tight now");
-        //     return;
-        // }
+        if (args.Length > 1) {
+            Console.WriteLine("Error: only one path supported tight now");
+            return;
+        }
 
-        // var path = args.Single();
+        var path = args.Single();
 
-        // if (!File.Exists(path)) {
-        //     Console.WriteLine($"ERROR: file {path} does not exist.");
-        //     return;
-        // }
+        if (!File.Exists(path)) {
+            Console.WriteLine($"ERROR: file {path} does not exist.");
+            return;
+        }
 
-        var syntaxTree = SyntaxTree.Load("/home/ludovic/Documents/projects/rhino/samples/hello.ri");
+        var syntaxTree = SyntaxTree.Load(path);
         var compilation = new Compilation(syntaxTree);
         var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
 
