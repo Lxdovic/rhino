847c1f7bd3288762005b603f4db3182eb351a685
ludovic <ludovicdebever0@gmail.com>
2024-04-28 13:37:35 +0200

    chore: add sample and compiler project

--- Rhino/IO/TextWriterExtensions.cs
+++ Rhino/IO/TextWriterExtensions.cs
@@ -1,9 +1,11 @@
 using System.CodeDom.Compiler;
+using Rhino.CodeAnalysis;
 using Rhino.CodeAnalysis.Syntax;
+using Rhino.CodeAnalysis.Text;
 
 namespace Rhino.IO;
 
-internal static class TextWriterExtensions {
+public static class TextWriterExtensions {
     private static bool IsConsoleOut(this TextWriter writer) {
         if (writer == Console.Out) return true;
         if (writer is IndentedTextWriter iw && iw.InnerWriter.IsConsoleOut()) return true;
@@ -61,5 +63,40 @@ internal static class TextWriterExtensions {
         writer.SetForeground(ConsoleColor.DarkGray);
         writer.Write(text);
         writer.ResetColor();
+    }
+
+    public static void WriteDiagnostics(this TextWriter writer, IEnumerable<Diagnostic> diagnostics,
+        SyntaxTree syntaxTree) {
+        foreach (var diagnostic in diagnostics.OrderBy(diag => diag.Span.Start).ThenBy(diag => diag.Span.Length)) {
+            var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+            var line = syntaxTree.Text.Lines[lineIndex];
+            var lineNumber = lineIndex + 1;
+            var character = diagnostic.Span.Start - line.Start + 1;
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($"({lineNumber}, {character}): ");
+            Console.WriteLine(diagnostic);
+            Console.ResetColor();
+
+            var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+            var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+            var prefix = syntaxTree.Text.ToString(prefixSpan);
+            var error = syntaxTree.Text.ToString(diagnostic.Span);
+            var suffix = syntaxTree.Text.ToString(suffixSpan);
+
+            Console.Write("    ");
+            Console.Write(prefix);
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(error);
+            Console.ResetColor();
+
+            Console.Write(suffix);
+
+            Console.WriteLine();
+        }
     }
 }
---
+++ rc/Program.cs
@@ -0,0 +1,29 @@
+﻿using Rhino.CodeAnalysis;
+using Rhino.CodeAnalysis.Symbols;
+using Rhino.CodeAnalysis.Syntax;
+using Rhino.IO;
+
+namespace rc;
+
+internal class Program {
+    private static void Main(string[] args) {
+        if (args.Length == 0) {
+            Console.WriteLine("Usage: rc <filename>");
+            return;
+        }
+
+        if (args.Length > 1) {
+            Console.WriteLine("Error: only one path supported tight now");
+            return;
+        }
+
+        var path = args.Single();
+        var text = File.ReadAllText(path);
+        var syntaxTree = SyntaxTree.Parse(text);
+        var compilation = new Compilation(syntaxTree);
+        var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+        if (result.Diagnostics.Any()) Console.Error.WriteDiagnostics(result.Diagnostics, syntaxTree);
+        else if (result.Value is not null) Console.WriteLine(result.Value);
+    }
+}
